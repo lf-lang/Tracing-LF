8f12eb129716861f2dde115ddf67046815f5a09a
Elocien <lucianmcintyre@gmail.com>
2021-12-13 17:11:25 +0100

    first steps to order data in dict

--- yamlParser.py
+++ yamlParser.py
@@ -1,2 +1,40 @@
 import yaml
 
+
+class analyse_lf:
+    """ Can be used to parse yaml files generated from LF programs (currently only in the C++ target) with the 'export-to-yaml' option set to true. \n
+        Allows parsing of the yaml file"""
+        
+    def __init__(self, filepath):
+        # Open yaml file and parse with pyyaml
+        self.data = yaml.load(open(filepath), Loader=yaml.FullLoader)
+        
+        self.reaction_dict = []
+    
+        # 
+        reactor_instances = self.data['all_reactor_instances']
+    
+        # Delete the first item in the dict
+        del reactor_instances[next(iter(reactor_instances))]
+        
+               
+        # Iterate through dict
+        for reactor in reactor_instances.items():
+            reactor_name = reactor[0]
+            for reaction in reactor[1]["reactions"]:
+                print(reaction["priority"])
+                reaction_name = reactor_name + "." + reaction["name"]
+                self.reaction_dict[str(reaction_name)] = []
+                
+            
+            
+            
+            
+    
+        
+        
+test = analyse_lf("YamlFiles/ReflexGame.yaml")
+        
+
+
+        
