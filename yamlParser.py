34a57d45a3c3bffcbeb825ede666a2bc86e0df07
Elocien <lucianmcintyre@gmail.com>
2021-12-13 19:38:13 +0100

    yaml parser can now determine level and dependencies of a given node

--- yamlParser.py
+++ yamlParser.py
@@ -1,4 +1,5 @@
 import yaml
+from collections import defaultdict
 
 
 class analyse_lf:
@@ -6,25 +7,72 @@ class analyse_lf:
         Allows parsing of the yaml file"""
         
     def __init__(self, filepath):
+       
         # Open yaml file and parse with pyyaml
         self.data = yaml.load(open(filepath), Loader=yaml.FullLoader)
         
-        self.reaction_dict = []
+        # 
+        self.reaction_list = list()
+        self.dependecy_dict = defaultdict(list)
     
         # 
         reactor_instances = self.data['all_reactor_instances']
     
-        # Delete the first item in the dict
+        # Delete the first item in the dict (this is the top level reactor)
         del reactor_instances[next(iter(reactor_instances))]
         
                
-        # Iterate through dict
+        # Iterate through reactors and get reactions 
         for reactor in reactor_instances.items():
             reactor_name = reactor[0]
+            
+            # For each reaction, add it to the list of reactions
             for reaction in reactor[1]["reactions"]:
-                print(reaction["priority"])
-                reaction_name = reactor_name + "." + reaction["name"]
-                self.reaction_dict[str(reaction_name)] = []
+                
+                # Rename to include reactor name in reaction name
+                reaction["name"] = reactor_name + "." + reaction["name"]
+                self.reaction_list.append(reaction)
+              
+                
+        # Determine dependency list for each reaction
+        dependencies = self.data['reaction_dependencies']
+        
+        iterator = iter(dependencies)
+        for x in iterator:
+            from_node = x["from"]
+            self.dependecy_dict[from_node].append(next(iterator)["to"])
+            
+            
+        # find all nodes without dependecies and add these
+        for reaction in self.reaction_list:
+            reaction_name = reaction["name"]
+            if reaction_name not in self.dependecy_dict:
+                self.dependecy_dict[reaction_name] = []
+                
+
+        
+                
+   
+    def get_level(self, node_name):
+        """ Finds the level of a given node """
+        
+        for reaction in self.reaction_list:
+            if node_name == reaction["name"]:
+                return reaction["level"]
+        
+        raise ValueError("The given node does not exist. Possibly wrong name given")   
+    
+    
+    def get_dependencies(self, node_name):
+        """ Gets the list of dependencies for a given node """
+        if node_name in self.dependecy_dict:
+            return self.dependecy_dict[node_name]
+        else:
+            raise ValueError(
+                "The given node does not exist. Possibly wrong name given")
+            
+        
+        
                 
             
             
@@ -34,6 +82,8 @@ class analyse_lf:
         
         
 test = analyse_lf("YamlFiles/ReflexGame.yaml")
+print(test.get_level("ReflexGame.p.reaction_2"))
+print(test.get_dependencies("ReflexGame.p.reaction_2"))
         
 
 
