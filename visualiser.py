f939d88598f660a31238ca6ed8acd34962b22925
Elocien <lucianmcintyre@gmail.com>
2022-01-03 13:41:06 +0100

    categorical y_axis and init structure

--- visualiser.py
+++ visualiser.py
@@ -1,6 +1,8 @@
-#!/usr/bin/env python3
+
 from Parser.jsonParser import parse_json
 from Parser.yamlParser import parse_yaml
+
+from bokeh.plotting import figure, show, output_file
 
 
 
@@ -10,29 +12,48 @@ class visualiser:
     
     def __init__(self, yaml_filepath, json_filepath):
 
-        self.yaml_data = parse_yaml(yaml_filepath)
-        self.json_data = parse_json(json_filepath)
+        yaml_data = parse_yaml(yaml_filepath).reaction_dict
+        # {reactor : {reaction : {attribute : value}}}
+        
+        json_data = parse_json(json_filepath).data
+        # {reactor: {reaction: [list of executions of reactions]}}
+        
+        
+        
+        # Compile all related data into a single data structure
+        self.data = yaml_data
+        
+        for reactor, reactions in json_data.items():
+            if reactor not in yaml_data:
+                continue
+            
+            for reaction in reactions:
+                if reaction not in yaml_data[reactor]:
+                    continue
+                
+                self.data[reactor][reaction]["traces"] = json_data[reactor][reaction]
+        
+        
+        
         
     
     def build_graph(self):
+
+        # output to static HTML file
+        output_file("test.html")
         
-        # Retrieve the json data
-        data = self.json_data.raw_data
-        yaml_data = self.yaml_data.reaction_list
+        x = [1,2,3,4,5]
+        y_1 = [1, 2, 3, 4, 5]
+        y_2 = ["a", "b", "c", "d", "e"]
         
-        x = []
-        y = []
-        
-        # Iterate through reactors and their reactions
-        for reactor, reactions in data.items():
-            
-            # Add all reactors to y axis 
-            if reactor not in y:
-                y.append(reactor)
-                
-            # Iterate through all reactions
-            for reaction in reactions:
-                print(reaction)
+        p = figure(width=400, height=400, y_range=y_2)
+
+        # add a circle renderer with a size, color, and alpha
+        p.circle(x, y_2, size=20)
+
+        # show the results
+        show(p)
+
         
         # Add info to edges:
         # - trigger, triggered by, dependency?
