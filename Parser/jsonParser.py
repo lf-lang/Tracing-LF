1b56f6504110ac9e622fb0332839641ebe7ad1fe
Elocien <lucianmcintyre@gmail.com>
2022-01-03 10:56:46 +0100

    Execution(duration) traces properly formatted

--- Parser/jsonParser.py
+++ Parser/jsonParser.py
@@ -6,29 +6,37 @@ import json
 class parse_json:
     
     def __init__(self, filepath):
-        self.raw_data = json.load(open(filepath))
+        self.data = json.load(open(filepath))
         
         # Structure: 
         # {reactor: {reaction: [list of executions of reactions]}}
         
-        # dictionary containing all duration events
-        self.execution_dict = self.raw_data.pop("Execution")
+        # Dictionary containing all duration events
+        execution_dict = {}
+        
+        # Iterate and correctly name keys (0 = Thread 0, else Worker(x))
+        for key, value in self.data.pop("Execution").items():
+            if key == "0":
+                execution_dict["Thread 0"] = value
+            else:
+                execution_dict["Worker" + key] = value
+        
+        self.data["Exection"] = execution_dict
         
         
-        
-        # process the data
+        # Process the data
         
         # Determine start time, then remove this from all other timestamps
         start_time = 0
         
-        for reactor, reactions in self.raw_data.items():
+        for reactor, reactions in self.data.items():
             for reaction in reactions:
                 if reaction == "startup":
-                    start_time = self.raw_data[reactor][reaction][0]["ts"]
+                    start_time = self.data[reactor][reaction][0]["ts"]
                     break
         
         
-        for reactions in self.raw_data.values():
+        for reactions in self.data.values():
             for reaction in reactions.values():
                 for reaction_execution in reaction:
                     new_start_time = reaction_execution["ts"] - start_time
@@ -39,14 +47,13 @@ class parse_json:
        
     def get_reactions(self, reactor_name, reaction_name):
         """Returns the trace information of a given reactor and reaction"""
-        for reactor, reactions in self.raw_data.items():
+        for reactor, reactions in self.data.items():
             if reactor == reactor_name:
                 for reaction in reactions:
                     if reaction == reaction_name:
                         return reactions[reaction]
     
-            
-       
+
 
 
 
