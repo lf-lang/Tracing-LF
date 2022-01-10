5c380226f0b40fcd585b57c1083b9484a770c553
Elocien <lucianmcintyre@gmail.com>
2022-01-10 20:55:32 +0100

    Added new dicts to track reactor number and label

--- Parser/jsonParser.py
+++ Parser/jsonParser.py
@@ -8,6 +8,16 @@ class parse_json:
     def __init__(self, filepath):
         self.data = json.load(open(filepath))
         
+        # list of reactor names - [reactor.name0, reactor.name1, ...]
+        self.y_axis_labels = []
+
+        # list of axis labels to assign reactor names to y-values {reactor.name0 : 0, reactor.name1 : 1, ...}
+        # Solves problem: https://github.com/bokeh/bokeh/issues/6907
+        self.reactor_number = {}
+        
+        # reversal of reactor_number
+        self.number_label = {}
+        
         # Structure: 
         # {reactor: {reaction: [list of executions of reactions]}}
         
@@ -34,13 +44,27 @@ class parse_json:
                     start_time = self.data[reactor][reaction][0]["ts"]
                     break
         
-        # Set new start time 
-        for reactions in self.data.values():
-            for reaction in reactions.values():
+        # Set new start time, find all reaction names and assign number to each reaction
+        for reactor, reactions in self.data.items():
+            for name, reaction in reactions.items():
+                
                 for reaction_execution in reaction:
                     new_start_time = reaction_execution["ts"] - start_time
                     reaction_execution["ts"] = new_start_time
                     
+                if reactor != "Execution":
+                    reactor_name = reactor + "." + name
+
+                    # Assigns current y-coord to reactor name
+                    self.number_label[len(
+                        self.y_axis_labels)] = reactor_name
+
+                    # Add reactor name to list
+                    self.y_axis_labels.append(reactor_name)
+
+                    
+        # reverse number_label dict
+        self.reactor_number = {v: k for k, v in self.number_label.items()}
 
 
        
@@ -51,10 +75,3 @@ class parse_json:
                 for reaction in reactions:
                     if reaction == reaction_name:
                         return reactions[reaction]
-    
-
-    
-                
-    
-
-
