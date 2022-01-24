6ae0339cf8fc9b3b2de8ab9a68b9046fbdaaa610
Elocien <lucianmcintyre@gmail.com>
2022-01-24 18:24:15 +0100

    Big parsing changes and updated vis to accomodate

---
+++ Parser/parse_files.py
@@ -0,0 +1,234 @@
+import json
+import yaml
+
+
+class parser:
+    
+    def parse(self, yaml_filepath, json_filepath):
+
+        # Variables
+        
+        yaml_data = self.parse_yaml(yaml_filepath)
+        json_data = self.parse_json(json_filepath)
+        
+        # list of reactor names - [reactor.name0, reactor.name1, ...]
+        self.y_axis_labels = []
+
+        # list of axis labels to assign reactor names to y-values {reactor.name0 : 0, reactor.name1 : 1, ...}
+        # Solves problem: https://github.com/bokeh/bokeh/issues/6907
+        self.reactor_number = {}
+        
+        # reversal of reactor_number
+        self.number_label = {}
+        
+        # Get reactor number and label, as well as setting timestamps to begin at 0
+        
+        # Get first reaction 
+        start_time = json_data["traceEvents"][0]["ts"]
+        
+        for item in json_data["traceEvents"]:
+            
+            # set new start time
+            current_start_time = item["ts"]
+            item["ts"] = current_start_time - start_time
+            
+            # add reaction to reactions
+            reaction_name = item["reactor"] + "." + item["reaction"]
+            
+            if reaction_name not in self.y_axis_labels:
+                self.number_label[len(
+                    self.y_axis_labels)] = reaction_name
+
+                # Add reactor name to list
+                self.y_axis_labels.append(reaction_name)
+                
+        # inverse of number_label dictionary
+        self.reactor_number = {v: k for k, v in self.number_label.items()}
+        
+        # Dictionary containing all compiled data for each instantaneous event execution (reactions)
+        # Note: time_start is used for the x-axis, y-axis is the y value which is later substituted for a reaction name
+        self.ordered_inst_events_reactions = {"name": [], "reactor": [], "reaction": [], "time_start": [], "time_end": [],
+                                              "trace_event_type": [], "y_axis": [], "priority": [], "level": [], "triggers": [], "effects": []}
+        
+        # Dictionary containing all compiled data for each instantaneous event execution
+        self.ordered_inst_events_actions = {"name": [], "reactor": [], "reaction": [], "time_start": [], "time_end": [],
+                                         "trace_event_type": [], "y_axis": []}
+
+        # Dictionary containing all compiled data for each execution event execution
+        # x_multi_line and y_multi_line contain nested lists with start and end x and y values respectively. These are used to draw the multilines
+        self.ordered_exe_events = {"name": [], "time_start": [], "time_end": [], "trace_event_type": [], "priority": [],
+                                        "level": [], "triggers": [], "effects": [], "x_multi_line": [], "y_multi_line": [], "y_axis": []}
+
+        # YAML attribute list
+        attribute_list = ["priority", "level", "triggers", "effects"]
+
+
+        # Iterate through all recorded reactions and order into dicts
+        json_data_iterator = iter(json_data["traceEvents"])
+        for item in json_data_iterator:
+            
+            reactor_name = item["reactor"]
+            reaction_name = item["reaction"]
+            reactor_reaction_name = reactor_name + "." + reaction_name
+            time_start = item["ts"]
+            
+            
+            # Execution events (not instantaneous)
+            if item["reactor"] == "Execution":
+                
+                current_reactor_name = "" 
+                current_reaction_name = ""
+                
+                try:
+                    x, y, z = item["name"].split(".", 2)
+                    current_reactor_name = x + "." + y
+                    current_reaction_name = z
+                
+                except Exception:
+                    x, z = item["name"].split(".", 1)
+                    current_reactor_name = x
+                    current_reaction_name = z
+                
+                reaction_yaml_data = yaml_data[current_reactor_name][current_reaction_name]
+
+                # JSON Data
+                self.ordered_exe_events["name"].append(reactor_reaction_name)
+                self.ordered_exe_events["time_start"].append(time_start)
+                self.ordered_exe_events["y_axis"].append(self.reactor_number[reactor_reaction_name])
+                self.ordered_exe_events["trace_event_type"].append("execution")
+
+                # YAML Data
+                attribute_list = ["priority",
+                                    "level", "triggers", "effects"]
+                # YAML Data
+                for attribute in attribute_list:
+                    if attribute in reaction_yaml_data:
+                        self.ordered_exe_events[attribute].append(
+                            reaction_yaml_data[attribute])
+                    else:
+                        self.ordered_exe_events[attribute].append("n.a.")
+
+
+                # Get end time of event by going to the next json event (start and end events are distinct)
+                try:
+                    next_reaction = next(json_data_iterator)
+                    self.ordered_exe_events["time_end"].append(
+                        next_reaction["ts"])
+
+                # If no end event exists, add end time as same timestamp (i.e. the last element of time_start)
+                except StopIteration as e:
+                    self.ordered_exe_events["time_end"].append(
+                        self.ordered_exe_events["time_start"][-1])
+
+            # All other events from the trace
+            else:
+                
+                reaction_yaml_data = yaml_data[reactor_name][reaction_name]
+                
+                # If the the event is an action, add to self.ordered_inst_events_actions
+                if "type" in reaction_yaml_data and (reaction_yaml_data["type"] == "logical action" or reaction_yaml_data["type"] == "physical action"):
+                    # JSON Data
+                    self.ordered_inst_events_actions["name"].append(reactor_reaction_name)
+                    self.ordered_inst_events_actions["reactor"].append(reactor_name)
+                    self.ordered_inst_events_actions["reaction"].append(reaction_name)
+                    self.ordered_inst_events_actions["time_start"].append(time_start)
+                    self.ordered_inst_events_actions["time_end"].append(time_start)   #same for instant events
+                    self.ordered_inst_events_actions["trace_event_type"].append("instant")
+                    self.ordered_inst_events_actions["y_axis"].append(self.reactor_number[reactor_reaction_name])
+
+                
+                # If the the event is a reaction, add to self.ordered_inst_events_reactions
+                else:
+                    # JSON Data
+                    self.ordered_inst_events_reactions["name"].append(reactor_reaction_name)
+                    self.ordered_inst_events_reactions["reactor"].append(reactor_name)
+                    self.ordered_inst_events_reactions["reaction"].append(reaction_name)
+                    self.ordered_inst_events_reactions["time_start"].append(time_start)
+                    self.ordered_inst_events_reactions["time_end"].append(time_start)  # start and end is the same for instant events
+                    self.ordered_inst_events_reactions["trace_event_type"].append("instant")
+                    self.ordered_inst_events_reactions["y_axis"].append(self.reactor_number[reactor_reaction_name])
+
+                    # YAML Data
+                    for attribute in attribute_list:
+                        if attribute in reaction_yaml_data:
+                            self.ordered_inst_events_reactions[attribute].append(reaction_yaml_data[attribute])
+                        else:
+                            self.ordered_inst_events_reactions[attribute].append("n.a.")
+                    
+                    if len(self.ordered_inst_events_reactions["name"]) != len(self.ordered_inst_events_reactions["priority"]):
+                        print("lol")
+         
+         
+            
+        # order data for multiline graph
+        for start_time, end_time in zip(self.ordered_exe_events["time_start"], self.ordered_exe_events["time_end"]):
+            self.ordered_exe_events["x_multi_line"].append(
+                [start_time, end_time])
+
+        for y_value in self.ordered_exe_events["y_axis"]:
+            self.ordered_exe_events["y_multi_line"].append(
+                [y_value, y_value])
+                
+        
+
+
+
+    def parse_yaml(self, filepath):
+        # Structure: Nested dict of reactions
+        # {reactor : {reaction : {attribute : value}}}
+        reaction_dict= {}
+
+        # Open yaml file and parse with pyyaml
+        yaml_data = yaml.load(open(filepath), Loader=yaml.FullLoader)
+
+        # Iterate through reactors and get reactions
+        for reactor, items in yaml_data['all_reactor_instances'].items():
+
+            if reactor not in reaction_dict:
+                reaction_dict[reactor] = {}
+
+            # For each reaction, add it to the list of reactions
+            if items["reactions"] is not None:
+                for reaction in items["reactions"]:
+
+                    reaction_dict[reactor][reaction["name"]] = reaction
+
+            if items["triggers"] is not None:
+                for trigger in items["triggers"]:
+
+                    # rename trigger_of and event_of for clarity
+                    trigger["effects"] = trigger.pop("effect_of")
+                    trigger["triggers"] = trigger.pop("trigger_of")
+
+                    reaction_dict[reactor][trigger["name"]] = trigger
+
+        return reaction_dict
+    
+    
+    
+    def parse_json(self, filepath):
+        data = json.load(open(filepath))
+        return data
+
+
+    def get_ordered_inst_events_reactions(self):
+        return self.ordered_inst_events_reactions
+
+    def get_ordered_inst_events_actions(self):
+        return self.ordered_inst_events_actions
+    
+    def get_ordered_exe_events(self):
+        return self.ordered_exe_events
+    
+    def get_y_axis_labels(self):
+        return self.y_axis_labels
+    
+    def get_number_label(self):
+        return self.number_label
+    
+
+p = parser()
+p.parse("yaml_files/Throughput.yaml",
+        "traces/throughput_new.json")
+
+print("done")
--- visualiser.py
+++ visualiser.py
@@ -1,6 +1,6 @@
 
-from Parser.jsonParser import parse_json
-from Parser.yamlParser import parse_yaml
+from Parser.parse_files import parser
+
 
 from bokeh.io import output_file, show
 from bokeh.models import ColumnDataSource, HoverTool
@@ -11,119 +11,18 @@ from bokeh.transform import jitter
 class visualiser:
     
     def __init__(self, yaml_filepath, json_filepath):
+        data_parser = parser()
+        data_parser.parse(yaml_filepath, json_filepath)
         
-        yaml = parse_yaml(yaml_filepath)
-        json = parse_json(json_filepath)
+        self.ordered_exe_events = data_parser.get_ordered_exe_events()
+        self.ordered_inst_events_reactions = data_parser.get_ordered_inst_events_reactions()
+        self.ordered_inst_events_actions = data_parser.get_ordered_inst_events_actions()
         
-        self.graph_name = yaml_filepath.split(".")[0] + "_Trace"
-        
-        # [reactor.name0, reactor.name1, ...]
-        self.y_axis_labels = json.y_axis_labels
-        
-        # {reactor.name0 : 0, reactor.name1 : 1, ...}
-        self.reactor_number = json.reactor_number
-
-        # reversal of reactor_number
-        self.number_label = json.number_label
-
-        yaml_data = yaml.reaction_dict
-        # {reactor : {reaction : {attribute : value}}}
-        
-        json_data = json.data
-        # {reactor: {reaction: [list of executions of reactions]}}
+        self.y_axis_labels = data_parser.get_y_axis_labels()
+        self.number_label = data_parser.get_number_label()
         
         
-        # Dictionary containing all compiled data for each instantaneous event execution 
-        # Note: time_start is used for the x-axis, y-axis is the y value which is later substituted for a reaction name
-        self.ordered_inst_events_dict = {"name": [], "reactor": [], "reaction": [], "time_start": [], "time_end": [], 
-                                         "trace_event_type": [], "priority": [], "level": [], "triggers": [], "effects": [], "y_axis": []}
-        
-        # Dictionary containing all compiled data for each execution event execution
-        # x_multi_line and y_multi_line contain nested lists with start and end x and y values respectively. These are used to draw the multilines
-        self.ordered_exe_events_dict = {"name": [], "time_start": [], "time_end": [], "trace_event_type": [], "priority": [], 
-                                        "level": [], "triggers": [], "effects": [], "x_multi_line": [], "y_multi_line": [], "y_axis": []}
-
-        
-        # Iterate through all recorded reactions and order into dicts
-        for reactor, reactions in json_data.items():
-            
-            # Execution events (not instantaneous)
-            if reactor == "Execution":
-                for reaction in reactions:
-                    execution_events_iter = iter(json_data[reactor][reaction])
-                    for reaction_instance in execution_events_iter:
-                    
-                        # JSON Data
-                        self.ordered_exe_events_dict["name"].append(reaction_instance["name"])
-                        self.ordered_exe_events_dict["time_start"].append(reaction_instance["ts"])
-                        
-                        # x - main reactor name
-                        # y - reactor name                      E.g. ReflexGame.g.reaction1
-                        # z - reaction name
-                        x,y,z = reaction_instance["name"].split(".", 2)
-                        
-                        current_reactor_name = x + "." + y
-                        current_reaction_name = z
-                        
-                        self.ordered_exe_events_dict["y_axis"].append(
-                            self.reactor_number[reaction_instance["name"]])
-                        
-                        current_reaction = yaml_data[current_reactor_name][current_reaction_name]
-                        
-                        self.ordered_exe_events_dict["trace_event_type"].append("execution")
-                            
-                        # YAML Data
-                        attribute_list = ["priority", "level", "triggers", "effects"]
-                        for attribute in attribute_list:
-                            if attribute in current_reaction:
-                                self.ordered_exe_events_dict[attribute].append(
-                                    current_reaction[attribute])
-                            else:
-                                self.ordered_exe_events_dict[attribute].append(
-                                    "n.a.")
-                        
-                        # Get end time of event by going to the next json event (start and end events are distinct)
-                        try:
-                            next_reaction = next(execution_events_iter)
-                            self.ordered_exe_events_dict["time_end"].append(
-                                next_reaction["ts"])
-                        
-                        # If no end event exists, add end time as same timestamp (i.e. the last element of time_start)
-                        except StopIteration as e:
-                            self.ordered_exe_events_dict["time_end"].append(
-                                self.ordered_exe_events_dict["time_start"][-1])
-                    
-            # All other events from the trace 
-            else:
-                for reaction in reactions:
-                    for reaction_instance in json_data[reactor][reaction]:
-                        
-                        # JSON Data
-                        self.ordered_inst_events_dict["name"].append(
-                            reactor + "." + reaction)
-                        self.ordered_inst_events_dict["reactor"].append(reactor)
-                        self.ordered_inst_events_dict["reaction"].append(reaction)
-                        self.ordered_inst_events_dict["time_start"].append(
-                            reaction_instance["ts"])
-                        self.ordered_inst_events_dict["time_end"].append(
-                            reaction_instance["ts"])
-                        self.ordered_inst_events_dict["trace_event_type"].append(
-                            "instant")
-                        self.ordered_inst_events_dict["y_axis"].append(
-                            self.reactor_number[reactor + "." + reaction])
-                        
-                        # YAML Data
-                        current_reaction = yaml_data[reactor][reaction]
-                        
-                        attribute_list = ["priority", "level", "triggers", "effects"]
-                        for attribute in attribute_list:
-                            if attribute in current_reaction:
-                                self.ordered_inst_events_dict[attribute].append(
-                                    current_reaction[attribute])
-                            else:
-                                self.ordered_inst_events_dict[attribute].append("n.a.")
-        
-    
+        self.graph_name = "Trace"
     
     
     def build_graph(self):
@@ -152,20 +51,10 @@ class visualiser:
         
         # -------------------------------------------------------------------
         # All execution events
-        
-        # order data for multiline graph
-        for start_time, end_time in zip(self.ordered_exe_events_dict["time_start"], self.ordered_exe_events_dict["time_end"]):
-            self.ordered_exe_events_dict["x_multi_line"].append(
-                [start_time, end_time])
-
-        for y_value in self.ordered_exe_events_dict["y_axis"]:
-            self.ordered_exe_events_dict["y_multi_line"].append(
-                [y_value, y_value])
 
         
         # data source
-        source_exec_events = ColumnDataSource(
-            self.ordered_exe_events_dict)
+        source_exec_events = ColumnDataSource(self.ordered_exe_events)
         
             
         # https://docs.bokeh.org/en/latest/docs/user_guide/plotting.html#line-glyphs
@@ -176,21 +65,31 @@ class visualiser:
         # -------------------------------------------------------------------      
         
         # Execution event markers 
-        exe_x_marker = [((x1 + x2)/2) for x1, x2 in self.ordered_exe_events_dict["x_multi_line"]] 
-        exe_y_marker = self.ordered_exe_events_dict["y_axis"]
+        exe_x_marker = [((x1 + x2)/2)
+                        for x1, x2 in self.ordered_exe_events["x_multi_line"]]
+        exe_y_marker = self.ordered_exe_events["y_axis"]
         
         exe_marker = p.diamond(exe_x_marker, exe_y_marker, color="mediumspringgreen",
                     size = 10, legend_label = "Execution Event Markers", muted_alpha = 0.2)
         
         # -------------------------------------------------------------------
         
-        # All instantaneous events
-        source_inst_events = ColumnDataSource(self.ordered_inst_events_dict)
+        # All instantaneous events that are reactions
+        source_inst_events = ColumnDataSource(self.ordered_inst_events_reactions)
 
-        inst_hex = p.hex(x='time_start', y=jitter('y_axis', width=0, mean=0.25), fill_color="hotpink",
+        inst_reaction_hex = p.hex(x='time_start', y=jitter('y_axis', width=0, mean=0.25), fill_color="hotpink",
                          size=10, source=source_inst_events, legend_label="Instantaneous Events", muted_alpha=0.2)
 
         # -------------------------------------------------------------------
+        
+        # All instantaneous events that are actions
+        source_inst_events = ColumnDataSource(self.ordered_inst_events_actions)
+
+        inst_action_hex = p.hex(x='time_start', y=jitter('y_axis', width=0, mean=0.25), fill_color=" cadetblue",
+                         size=10, source=source_inst_events, legend_label="Instantaneous Events", muted_alpha=0.2)
+
+        # -------------------------------------------------------------------
+
 
         p.legend.location = "top_left"
 
@@ -207,7 +106,7 @@ class visualiser:
         
         # Hover tool only for instantaneous events and execution event lines (so that markers for exe events dont also have a tooltip)
         hover_tool = HoverTool(tooltips=TOOLTIPS, renderers=[
-                               inst_hex, exe_line])
+                               inst_reaction_hex, exe_line])
         p.add_tools(hover_tool)
         
                 
@@ -217,7 +116,7 @@ class visualiser:
 
 
 if(__name__ == "__main__"):
-    vis = visualiser("yaml_files/ReflexGame.yaml",
-                     "traces/reflextrace_formatted.json")
+    vis = visualiser("yaml_files/Throughput.yaml",
+                     "traces/throughput_new.json")
 
     vis.build_graph()
